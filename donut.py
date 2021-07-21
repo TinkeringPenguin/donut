0c198d8f122c582fe37437f895d9571911820a95
Sa-So <60461699+Sa-So@users.noreply.github.com>
2021-07-22 04:29:03 +0530

    Update donut.py

--- donut.py
+++ donut.py
@@ -5,15 +5,20 @@ def main():
     a=0
     b=0
 
-    # for clearing console (windows and unix systems)
+    height=24
+    width=80
+    #height=int(input("Enter Screen Height : "))
+    #width=int(input("Enter Screen Width : "))
+    
+	# for clearing console (windows and unix systems)
     clear = "cls"
     if os.name == "posix":
         clear = "clear"
 
     os.system(clear)
     while True:
-        z = [0 for _ in range(7040)]
-        screen = [' ' for _ in range(1760)]
+        z = [0 for _ in range(4*height*width)]
+        screen = [' ' for _ in range(height*width)]
 
         j=0
         while j<6.28:
@@ -39,19 +44,23 @@ def main():
                 # 40 is the left screen shift
                 x = int(40+30*mess*(cosi*cosj2*cosB-t*sinB))
                 # 12 is the down screen shift
-                y = int(12+15*mess*(cosi*cosj2*sinB +t*cosB))
+                y = int(11+15*mess*(cosi*cosj2*sinB +t*cosB))
                 # all are casted to int, ie floored
-                o = int(x+80*y)
+                o = int(x+width*y)
+				# multiplying by 8 to bring in range 0-11 as 8*(sqrt(2))=11
+				# because we have 11 luminance characters
                 N = int(8*((sinj*sinA-sini*cosj*cosA)*cosB-sini*cosj*sinA-sinj*cosA-cosi *cosj*sinB))
-
-                if 0<y<22 and 0<x<80 and z[o] < mess:
+				# if x,y inside screen and previous z-buffer is < mess 
+				# i.e. when z[o] is 0 or the prev point is behind the new point
+				# so we change it to the point nearer to the eye/ above prev point 
+                if 0<y<height and 0<x<width and z[o] < mess:
                     z[o]=mess
                     screen[o]=".,-~:;=!*#$@"[N if N>0 else 0]
 
         # prints
         os.system(clear)
         for index, char in enumerate(screen):
-            if index % 80 == 0:
+            if index % width == 0:
                 print()
             else:
                 print(char, end='')
